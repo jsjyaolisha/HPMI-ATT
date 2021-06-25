e7a5b7ed45d5b70592526aa474719ee7d1d720d3
yaolisha <jsjyaolisha@163.com>
2021-06-25 20:54:26 +0800

    An HPMI module based on spatial and channel-based mixed attention mechanism is embedded in VGG-16 network

---
+++ demo/.history/src/model_20210422104359.py
@@ -0,0 +1,329 @@
+
+
+
+
+
+# model class
+class VGG16_HPMI(torch.nn.Module):
+
+  # init function
+  def __init__(self, model, num_classes=2):
+    super().__init__()
+
+    # pool layer
+    self.pool = torch.nn.Sequential(torch.nn.MaxPool2d(kernel_size=2, stride=2))
+
+    # spatial attention
+    self.spatial_attention = torch.nn.Sequential(
+        torch.nn.Conv2d(2, 1, kernel_size=7, padding=3, stride=1),
+        torch.nn.BatchNorm2d(1),
+        torch.nn.Sigmoid()
+    )
+
+    # spatial attention
+    self.spatial_attention2 = torch.nn.Sequential(
+        torch.nn.Conv2d(64, 1, kernel_size=7, padding=3, stride=1),
+        torch.nn.BatchNorm2d(1),
+        torch.nn.Sigmoid()
+    )
+    # spatial attention
+    self.spatial_attention3 = torch.nn.Sequential(
+        torch.nn.Conv2d(128, 1, kernel_size=7, padding=3, stride=1),
+        torch.nn.BatchNorm2d(1),
+        torch.nn.Sigmoid()
+    )
+    # spatial attention
+    self.spatial_attention4 = torch.nn.Sequential(
+        torch.nn.Conv2d(256, 1, kernel_size=7, padding=3, stride=1),
+        torch.nn.BatchNorm2d(1),
+        torch.nn.Sigmoid()
+    )
+    # spatial attention
+    self.spatial_attention5 = torch.nn.Sequential(
+        torch.nn.Conv2d(512, 1, kernel_size=7, padding=3, stride=1),
+        torch.nn.BatchNorm2d(1),
+        torch.nn.Sigmoid()
+    )
+
+    # channel attention
+    self.max_pool_1 = torch.nn.Sequential(torch.nn.MaxPool2d(kernel_size=224, stride=224))
+    self.max_pool_2 = torch.nn.Sequential(torch.nn.MaxPool2d(kernel_size=112, stride=112))
+    self.max_pool_3 = torch.nn.Sequential(torch.nn.MaxPool2d(kernel_size=56, stride=56))
+    self.max_pool_4 = torch.nn.Sequential(torch.nn.MaxPool2d(kernel_size=28, stride=28))
+    self.max_pool_5 = torch.nn.Sequential(torch.nn.MaxPool2d(kernel_size=14, stride=14))
+    self.avg_pool_1 = torch.nn.Sequential(torch.nn.AvgPool2d(kernel_size=224, stride=224))
+    self.avg_pool_2 = torch.nn.Sequential(torch.nn.AvgPool2d(kernel_size=112, stride=112))
+    self.avg_pool_3 = torch.nn.Sequential(torch.nn.AvgPool2d(kernel_size=56, stride=56))
+    self.avg_pool_4 = torch.nn.Sequential(torch.nn.AvgPool2d(kernel_size=28, stride=28))
+    self.avg_pool_5 = torch.nn.Sequential(torch.nn.AvgPool2d(kernel_size=14, stride=14))
+
+    # features
+    self.features_1 = torch.nn.Sequential(*list(model.features.children())[:3])
+    self.features_2 = torch.nn.Sequential(*list(model.features.children())[3:6])
+    self.features_3 = torch.nn.Sequential(*list(model.features.children())[7:10])
+    self.features_4 = torch.nn.Sequential(*list(model.features.children())[10:13])
+    self.features_5 = torch.nn.Sequential(*list(model.features.children())[14:17])
+    self.features_6 = torch.nn.Sequential(*list(model.features.children())[17:20])
+    self.features_7 = torch.nn.Sequential(*list(model.features.children())[20:23])
+    self.features_8 = torch.nn.Sequential(*list(model.features.children())[24:27])
+    self.features_9 = torch.nn.Sequential(*list(model.features.children())[27:30])
+    self.features_10 = torch.nn.Sequential(*list(model.features.children())[30:33])
+    self.features_11 = torch.nn.Sequential(*list(model.features.children())[34:37])
+    self.features_12 = torch.nn.Sequential(*list(model.features.children())[37:40])
+    self.features_13 = torch.nn.Sequential(*list(model.features.children())[40:43])
+
+    self.avgpool = nn.AdaptiveAvgPool2d(7)
+
+    # classifier
+    self.classifier = torch.nn.Sequential(
+        torch.nn.Linear(25088, 4096),
+        torch.nn.ReLU(inplace=True),
+        torch.nn.Dropout(),
+        torch.nn.Linear(4096, 4096),
+        torch.nn.ReLU(inplace=True),
+        torch.nn.Dropout(),
+        torch.nn.Linear(4096, 7)
+    )
+
+
+  # forward
+  def forward(self, x):
+    x = self.features_1(x)
+    # CA
+    scale_CA1 = torch.nn.functional.sigmoid(self.max_pool_1(x) + self.avg_pool_1(x)).expand_as(x)
+    x1 = x * scale_CA1
+    scale_CA2 = torch.cat((torch.max(x1, 1)[0].unsqueeze(1), torch.mean(x1, 1).unsqueeze(1)), dim=1)
+    # SA
+    scale_SA1 = self.spatial_attention(scale_CA2)
+    x1 =x1 * scale_SA1
+    
+    # SA
+    scale_SA2 = self.spatial_attention2(x)
+    x2 =scale_SA2 * scale_CA1
+    x= x1*x2
+
+
+    x = self.features_2(x)
+    # CA
+    scale_CA1 = torch.nn.functional.sigmoid(self.max_pool_1(x) + self.avg_pool_1(x)).expand_as(x)
+    x1 = x * scale_CA1
+    scale_CA2 = torch.cat((torch.max(x1, 1)[0].unsqueeze(1), torch.mean(x1, 1).unsqueeze(1)), dim=1)
+    # SA
+    scale_SA1 = self.spatial_attention(scale_CA2)
+    x1 =x1 * scale_SA1
+    
+    # SA
+    scale_SA2 = self.spatial_attention2(x)
+    x2 =scale_SA2 * scale_CA1
+    x= x1*x2
+    x = self.pool(x)
+    
+
+    x = self.features_3(x)
+    # CA
+    scale_CA1 = torch.nn.functional.sigmoid(self.max_pool_2(x) + self.avg_pool_2(x)).expand_as(x)
+    x1 = x * scale_CA1
+    scale_CA2 = torch.cat((torch.max(x1, 1)[0].unsqueeze(1), torch.mean(x1, 1).unsqueeze(1)), dim=1)
+    # SA
+    scale_SA1 = self.spatial_attention(scale_CA2)
+    x1 =x1 * scale_SA1
+    
+    # SA
+    scale_SA2 = self.spatial_attention3(x)
+    x2 =scale_SA2 * scale_CA1
+    x= x1*x2
+    
+
+    x = self.features_4(x)
+    # CA
+    scale_CA1 = torch.nn.functional.sigmoid(self.max_pool_2(x) + self.avg_pool_2(x)).expand_as(x)
+    x1 = x * scale_CA1
+    scale_CA2 = torch.cat((torch.max(x1, 1)[0].unsqueeze(1), torch.mean(x1, 1).unsqueeze(1)), dim=1)
+    # SA
+    scale_SA1 = self.spatial_attention(scale_CA2)
+    x1 =x1 * scale_SA1
+    
+    # SA
+    scale_SA2 = self.spatial_attention3(x)
+    x2 =scale_SA2 * scale_CA1
+    x= x1*x2
+    
+
+    x = self.pool(x)
+
+    x = self.features_5(x)
+    # CA
+    scale_CA1 = torch.nn.functional.sigmoid(self.max_pool_3(x) + self.avg_pool_3(x)).expand_as(x)
+    x1 = x * scale_CA1
+    scale_CA2 = torch.cat((torch.max(x1, 1)[0].unsqueeze(1), torch.mean(x1, 1).unsqueeze(1)), dim=1)
+    # SA
+    scale_SA1 = self.spatial_attention(scale_CA2)
+    x1 =x1 * scale_SA1
+    
+    # SA
+    scale_SA2 = self.spatial_attention4(x)
+    x2 =scale_SA2 * scale_CA1
+    x= x1*x2
+    
+
+    x = self.features_6(x)
+    # CA
+    scale_CA1 = torch.nn.functional.sigmoid(self.max_pool_3(x) + self.avg_pool_3(x)).expand_as(x)
+    x1 = x * scale_CA1
+    scale_CA2 = torch.cat((torch.max(x1, 1)[0].unsqueeze(1), torch.mean(x1, 1).unsqueeze(1)), dim=1)
+    # SA
+    scale_SA1 = self.spatial_attention(scale_CA2)
+    x1 =x1 * scale_SA1
+    
+    # SA
+    scale_SA2 = self.spatial_attention4(x)
+    x2 =scale_SA2 * scale_CA1
+    x= x1*x2
+    
+
+    x = self.features_7(x)
+    # CA
+    scale_CA1 = torch.nn.functional.sigmoid(self.max_pool_3(x) + self.avg_pool_3(x)).expand_as(x)
+    x1 = x * scale_CA1
+    scale_CA2 = torch.cat((torch.max(x1, 1)[0].unsqueeze(1), torch.mean(x1, 1).unsqueeze(1)), dim=1)
+    # SA
+    scale_SA1 = self.spatial_attention(scale_CA2)
+    x1 =x1 * scale_SA1
+    
+    # SA
+    scale_SA2 = self.spatial_attention4(x)
+    x2 =scale_SA2 * scale_CA1
+    x= x1*x2
+    
+    x = self.pool(x)
+
+    x = self.features_8(x)
+    # CA
+    scale_CA1 = torch.nn.functional.sigmoid(self.max_pool_4(x) + self.avg_pool_4(x)).expand_as(x)
+    x1 = x * scale_CA1
+    scale_CA2 = torch.cat((torch.max(x1, 1)[0].unsqueeze(1), torch.mean(x1, 1).unsqueeze(1)), dim=1)
+    # SA
+    scale_SA1 = self.spatial_attention(scale_CA2)
+    x1 =x1 * scale_SA1
+    
+    # SA
+    scale_SA2 = self.spatial_attention5(x)
+    x2 =scale_SA2 * scale_CA1
+    x= x1*x2
+    
+
+    x = self.features_9(x)
+    # CA
+    scale_CA1 = torch.nn.functional.sigmoid(self.max_pool_4(x) + self.avg_pool_4(x)).expand_as(x)
+    x1 = x * scale_CA1
+    scale_CA2 = torch.cat((torch.max(x1, 1)[0].unsqueeze(1), torch.mean(x1, 1).unsqueeze(1)), dim=1)
+    # SA
+    scale_SA1 = self.spatial_attention(scale_CA2)
+    x1 =x1 * scale_SA1
+    
+    # SA
+    scale_SA2 = self.spatial_attention5(x)
+    x2 =scale_SA2 * scale_CA1
+    x= x1*x2
+    
+
+    x = self.features_10(x)
+    # CA
+    scale_CA1 = torch.nn.functional.sigmoid(self.max_pool_4(x) + self.avg_pool_4(x)).expand_as(x)
+    x1 = x * scale_CA1
+    scale_CA2 = torch.cat((torch.max(x1, 1)[0].unsqueeze(1), torch.mean(x1, 1).unsqueeze(1)), dim=1)
+    # SA
+    scale_SA1 = self.spatial_attention(scale_CA2)
+    x1 =x1 * scale_SA1
+    
+    # SA
+    scale_SA2 = self.spatial_attention5(x)
+    x2 =scale_SA2 * scale_CA1
+    x= x1*x2
+    
+    x = self.pool(x)
+
+    x = self.features_11(x)
+   # CA
+    scale_CA1 = torch.nn.functional.sigmoid(self.max_pool_5(x) + self.avg_pool_5(x)).expand_as(x)
+    x1 = x * scale_CA1
+    scale_CA2 = torch.cat((torch.max(x1, 1)[0].unsqueeze(1), torch.mean(x1, 1).unsqueeze(1)), dim=1)
+    # SA
+    scale_SA1 = self.spatial_attention(scale_CA2)
+    x1 =x1 * scale_SA1
+    
+    # SA
+    scale_SA2 = self.spatial_attention5(x)
+    x2 =scale_SA2 * scale_CA1
+    x= x1*x2
+    
+
+    x = self.features_12(x)
+    # CA
+    scale_CA1 = torch.nn.functional.sigmoid(self.max_pool_5(x) + self.avg_pool_5(x)).expand_as(x)
+    x1 = x * scale_CA1
+    scale_CA2 = torch.cat((torch.max(x1, 1)[0].unsqueeze(1), torch.mean(x1, 1).unsqueeze(1)), dim=1)
+    # SA
+    scale_SA1 = self.spatial_attention(scale_CA2)
+    x1 =x1 * scale_SA1
+    
+    # SA
+    scale_SA2 = self.spatial_attention5(x)
+    x2 =scale_SA2 * scale_CA1
+    x= x1*x2
+    
+
+    x = self.features_13(x)
+    # CA
+    scale_CA1 = torch.nn.functional.sigmoid(self.max_pool_5(x) + self.avg_pool_5(x)).expand_as(x)
+    x1 = x * scale_CA1
+    scale_CA2 = torch.cat((torch.max(x1, 1)[0].unsqueeze(1), torch.mean(x1, 1).unsqueeze(1)), dim=1)
+    # SA
+    scale_SA1 = self.spatial_attention(scale_CA2)
+    x1 =x1 * scale_SA1
+    
+    # SA
+    scale_SA2 = self.spatial_attention5(x)
+    x2 =scale_SA2 * scale_CA1
+    x= x1*x2
+    
+    x = self.pool(x)
+
+    x = self.avgpool(x)
+    x = x.view(x.shape[0], -1)
+    x = self.classifier(x)
+    return x
+
+def CNN3(input_shape=(48, 48, 1), n_classes=8):
+    """
+    :param input_shape:
+    :param n_classes:
+    :return:
+    """
+    # input
+    input_layer = Input(shape=input_shape)
+    x = Conv2D(32, (1, 1), strides=1, padding='same', activation='relu')(input_layer)
+    # block1
+    x = Conv2D(64, (3, 3), strides=1, padding='same')(x)
+    x = PReLU()(x)
+    x = Conv2D(64, (5, 5), strides=1, padding='same')(x)
+    x = PReLU()(x)
+    x = MaxPooling2D(pool_size=(2, 2), strides=2)(x)
+    # block2
+    x = Conv2D(64, (3, 3), strides=1, padding='same')(x)
+    x = PReLU()(x)
+    x = Conv2D(64, (5, 5), strides=1, padding='same')(x)
+    x = PReLU()(x)
+    x = MaxPooling2D(pool_size=(2, 2), strides=2)(x)
+    # fc
+    x = Flatten()(x)
+    x = Dense(2048, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(1024, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(n_classes, activation='softmax')(x)
+
+    model = Model(inputs=input_layer, outputs=x)
+    return model
+
+
---
+++ demo/.history/src/recognition_20210422111542.py
@@ -0,0 +1,126 @@
+"""
+desc: 本模块为表情预测处理模块
+"""
+import os
+import cv2
+import numpy as np
+from utils import index2emotion, expression_analysis, cv2_img_add_text
+from tensorflow.python.keras.layers import Input, Conv2D, MaxPooling2D, Dropout, BatchNormalization, Flatten, Dense, AveragePooling2D
+from tensorflow.python.keras.models import Model
+from tensorflow.python.keras.layers import PReLU
+
+
+def face_detect(img_path):
+    """
+    检测测试图片的人脸
+    :param img_path: 图片的完整路径
+    :return:
+    """
+
+    face_cascade = cv2.CascadeClassifier('./dataset/params/haarcascade_frontalface_alt.xml')
+    img = cv2.imread(img_path)
+
+    img_gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(
+        img_gray,
+        scaleFactor=1.1,
+        minNeighbors=1,
+        minSize=(30, 30)
+    )
+    return img, img_gray, faces
+
+
+def generate_faces(face_img, img_size=48):
+    """
+    将探测到的人脸进行增广
+    :param face_img: 灰度化的单个人脸图
+    :param img_size: 目标图片大小
+    :return:
+    """
+    face_img = face_img / 255.
+    face_img = cv2.resize(face_img, (img_size, img_size), interpolation=cv2.INTER_LINEAR)
+    resized_images = list()
+    resized_images.append(face_img[:, :])
+    resized_images.append(face_img[2:45, :])
+    resized_images.append(cv2.flip(face_img[:, :], 1))
+    # resized_images.append(cv2.flip(face_img[2], 1))
+    # resized_images.append(cv2.flip(face_img[3], 1))
+    # resized_images.append(cv2.flip(face_img[4], 1))
+    resized_images.append(face_img[0:45, 0:45])
+    resized_images.append(face_img[2:47, 0:45])
+    resized_images.append(face_img[2:47, 2:47])
+
+    for i in range(len(resized_images)):
+        resized_images[i] = cv2.resize(resized_images[i], (img_size, img_size))
+        resized_images[i] = np.expand_dims(resized_images[i], axis=-1)
+    resized_images = np.array(resized_images)
+    return resized_images
+
+
+def predict_expression(img_path, model):
+    """
+    对图中n个人脸进行表情预测
+    :param img_path:
+    :return:
+    """
+
+    border_color = (0, 0, 0)  # 黑框框
+    font_color = (255, 255, 255)  # 白字字
+
+    img, img_gray, faces = face_detect(img_path)
+    if len(faces) == 0:
+        return 'no', [0, 0, 0, 0, 0, 0, 0, 0]
+    # 遍历每一个脸
+    emotions = []
+    result_possibilitys = []
+    for (x, y, w, h) in faces:
+        face_img_gray = img_gray[y:y + h + 10, x:x + w + 10]
+        faces_img_gray = generate_faces(face_img_gray)
+        # 预测结果线性加权
+        results = model.predict(faces_img_gray)
+        result_sum = np.sum(results, axis=0).reshape(-1)
+        label_index = np.argmax(result_sum, axis=0)
+        emotion = index2emotion(label_index, 'en')
+        cv2.rectangle(img, (x - 10, y - 10), (x + w + 10, y + h + 10), border_color, thickness=2)
+        img = cv2_img_add_text(img, emotion, x + 30, y + 30, font_color, 20)
+        emotions.append(emotion)
+        result_possibilitys.append(result_sum)
+    if not os.path.exists("./output"):
+        os.makedirs("./output")
+    cv2.imwrite('./output/rst.png', img)
+    return emotions[0], result_possibilitys[0]
+
+def VGG16_HPMI(input_shape=(48, 48, 1), n_classes=8):
+    """
+    :param input_shape:
+    :param n_classes:
+    :return:
+    """
+    # input
+    input_layer = Input(shape=input_shape)
+    x = Conv2D(32, (1, 1), strides=1, padding='same', activation='relu')(input_layer)
+    # block1
+    x = Conv2D(64, (3, 3), strides=1, padding='same')(x)
+    x = PReLU()(x)
+    x = Conv2D(64, (5, 5), strides=1, padding='same')(x)
+    x = PReLU()(x)
+    x = MaxPooling2D(pool_size=(2, 2), strides=2)(x)
+    # block2
+    x = Conv2D(64, (3, 3), strides=1, padding='same')(x)
+    x = PReLU()(x)
+    x = Conv2D(64, (5, 5), strides=1, padding='same')(x)
+    x = PReLU()(x)
+    x = MaxPooling2D(pool_size=(2, 2), strides=2)(x)
+    # fc
+    x = Flatten()(x)
+    x = Dense(2048, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(1024, activation='relu')(x)
+    x = Dropout(0.5)(x)
+    x = Dense(n_classes, activation='softmax')(x)
+
+    model = Model(inputs=input_layer, outputs=x)
+    return model
+
+
+
